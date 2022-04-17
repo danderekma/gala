75fea32f97ee2315469f63528c6435ce7aa5ec38
Derek Ma <danderekma@gmail.com>
2022-04-17 05:30:37 -0700

    Changes

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,7 +1,19 @@
 import type { NextPage } from "next"
 
 const Home: NextPage = () => {
-  return <div>Home Screen</div>
+  return (
+    <div className="flex h-screen items-center justify-center bg-[url('/images/background.svg')]">
+      <div className="flex flex-col items-center">
+        <img className="w-64" src={"/images/text-logo.svg"} alt="Text Logo" />
+        <p className="m-5 font-sans text-lg text-gray">
+          Bridging communities through music.
+        </p>
+        <button className="hover: m-5 rounded-xl bg-light-purple py-4 px-8 font-sans text-sky-blue hover:border-[1px] hover:border-light-purple hover:bg-transparent hover:text-light-purple">
+          Start Your Journey!
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default Home
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -10,7 +10,16 @@ module.exports = {
       sans: ["Poppins Regular", ...defaultTheme.fontFamily.sans],
       serif: ["Made Dillan", ...defaultTheme.fontFamily.serif],
     },
-    extend: {},
+    extend: {
+      colors: {
+        magenta: "#FC19A3",
+        purple: "#9515E5",
+        "sky-blue": "#F4FAFF",
+        "light-purple": "#F1D2FB",
+        yellow: "#F9E067",
+        gray: "#7A6B6B",
+      },
+    },
   },
   plugins: [],
 }
