49d099e3564737d7baba0bddd0614a3b982ca7b8
Derek Ma <danderekma@gmail.com>
2022-04-16 17:11:07 -0700

    Create backend

--- client/pages/login.tsx
+++ client/pages/login.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next'
 const Login: NextPage = () => {
   return (
     <div>
-      <h1>Testing</h1>
+      <h1 className="text-xl text-purple-500">Login</h1>
     </div>
   )
 }
