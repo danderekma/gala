afce7ef40b33105748d22de37c8501d7e0ffa280
Derek Ma <danderekma@gmail.com>
2022-04-16 17:05:02 -0700

    Recreate backend

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,11 +1,7 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 
 const Home: NextPage = () => {
-  return (
-    <div>Home Screen</div>
-  )
+  return <div>Home Screen</div>
 }
 
 export default Home
---
+++ client/pages/login.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Login: NextPage = () => {
+  return (
+    <div>
+      <h1>Testing</h1>
+    </div>
+  )
+}
+
+export default Login
