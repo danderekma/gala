a69baf08e08a8bba8367d6bfff3f196c7ff4eb1c
Derek Ma <danderekma@gmail.com>
2022-04-16 21:58:33 -0700

    Changes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,9 +4,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    fontFamily: {
-      "made.dillan": ["made.dillan", "sans-serif"],
-    },
+    fontFamily: {},
     extend: {},
   },
   plugins: [],
